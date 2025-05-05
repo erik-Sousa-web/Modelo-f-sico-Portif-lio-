70e5a6085395f2f2ca7513589551ffac7a065558
Erik Sousa <kinhogr99@gmail.com>
2025-05-05 19:51:50 -0300

    Add files via upload

---
+++ sistema faculdade modelo fiisico.sql
@@ -0,0 +1,122 @@
+create database db_sistema_faculdade;
+use db_sistema_faculdade;
+
+
+create table tbl_cursos(
+	id int primary key auto_increment not null,
+	nome_curso varchar(45) not null,
+	semestres int not null,
+	valor decimal (10,2) not null,
+	descricao varchar (100),
+	unique index (id)
+);
+
+#relacção da tabela cursos com a tabela materias
+create table tbl_cursos_materias(
+	id int primary key auto_increment not null,
+    id_materias int,
+    id_cursos int,
+    
+    foreign key (id_materias) references tbl_materias (id),
+    foreign key (id_cursos) references tbl_cursos (id),
+    unique index (id)
+);
+
+#criação da tabela Materias com a FK da Tabela notas
+create table tbl_materias(
+	id int primary key auto_increment not null,
+    nome_materia varchar(45) not null,
+    cargar_horaria int not null,
+	id_notas int,
+    
+    foreign key (id_notas) 
+    references tbl_notas(id),
+    unique index (id)
+);
+
+#criação da tabela notas
+create table tbl_notas(
+	id int not null auto_increment primary key,
+    nota_aluno varchar (45) not null,
+    frequencia_aula varchar(45) not null,
+    unique index (id)
+);
+
+#criação da tabela professor com a FK da tabela Materias 
+create table tbl_professor(
+	id int primary key auto_increment not null,
+    nome_professor varchar(45) not null,
+    email varchar(45) not null,
+    data_contratacao date not null,
+    id_materias int,
+    
+    foreign key (id_materias)  
+    references tbl_materias (id),
+    unique index (id)
+);
+
+
+
+#criação da tabela turma
+create table tbl_turma(
+	id int auto_increment primary key not null,
+    horario varchar(15) not null,
+	
+    unique index (id)
+);
+
+#relação das tabelas turna com a tabeka materias 
+create table tbl_materias_turna(
+	id int auto_increment primary key not null,
+	id_turma int,
+	id_materias int,
+
+	foreign key (id_turma) references tbl_turma(id),
+	foreign key (id_materias) references tbl_materias (id),
+	unique index (id)
+);
+
+#criação da tablea endereço com a FK da tabela Aluno
+create table tbl_endereco(
+	id int auto_increment primary key not null,
+    cep varchar(45) not null,
+    logradouro varchar(45) not null,
+    numero varchar(45) not null,
+    bairro varchar(45) not null,
+    cidade varchar(45) not null,
+    estado varchar(45) not null,
+    id_aluno int,
+    
+    foreign key (id_aluno)
+    references tbl_aluno (id),
+    unique index (id)
+);
+
+#criação da tabela aluno 
+create table tbl_aluno(
+	id int primary key auto_increment not null,
+	nome_aluno varchar(45) not null,
+	cpf int not null,
+	data_nascimento date not null,
+	telefone varchar(45) not null,
+	curso varchar(45) not null,
+	matricula int not null,
+	email varchar(45),
+    
+    unique index (id)
+
+);
+
+#relação da tabela aluno com a tabela turma
+create table tbl_turma_aluno(
+id int primary key auto_increment not null,
+id_aluno int,
+id_turma int,
+
+foreign key (id_aluno) references tbl_aluno(id),
+foreign key (id_turma) references tbl_turma(id),
+unique index (id)
+);
+
+
+
